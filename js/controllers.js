9be0b4abf7e6d4dfb84172d012100626f34d3f03
hugotorreslinares <hugotorreslinares@gmail.com>
2015-10-22 02:51:58 +0300

    correccion en la edad del perro

--- js/controllers.js
+++ js/controllers.js
@@ -11,21 +11,34 @@
 	  $scope.jugador.energia=100;
 	  $scope.accion= null;
 	  $scope.valor=null;
+      $scope.fecha= new Date().getTime();
 	  
    var cargar = function(){
-  
+  var fechaHoy= new Date().getTime();
 	 if(localStorage.getItem('perro'))
 	  return JSON.parse(localStorage.getItem('perro'));
 	  else
 	    return {
 	   'name': 'Harry',
-	   'edad': 60 ,
+	   'edad': 1 ,
 	   'salud':40,
 	   'amor':40,
 	   'diversion':40,
-	   'comida':40	 
+	   'comida':40,
+        'fechaInicio':fechaHoy
 	   };
 	  };
+      
+      
+      var  secondsToString= function (seconds)
+{
+var numdays = Math.floor(seconds / 864000);
+var numhours = Math.floor((seconds % 864000) / 3600);
+var numminutes = Math.floor(((seconds % 864000) % 3600) / 60);
+var numseconds = ((seconds % 864000) % 3600) % 60;
+    $scope.numdays= numdays;
+return numdays + " days " + numhours + " hours " + numminutes + " minutes " + numseconds + " seconds";
+};
 	  
 	
 	$scope.borrar=function(){
@@ -38,18 +51,19 @@
  
   $scope.perro = cargar();
   var envejecer = function(dias){
-			$scope.perro.edad+=dias;
-			
+			$scope.perro.edadString =secondsToString($scope.perro.ultimo-$scope.perro.fechaInicio );
+      $scope.perro.edad= $scope.numdays;
+      console.log( $scope.numdays);
 			};
-
 
 	  $scope.guardar= function(){
 		  
 		  $scope.jugador.nivel ++;  
 		  $scope.jugador.energia --;
 		  var edad =parseInt($scope.perro.edad);
-		  var time =new Date();
+		  var time =new Date().getTime();
 		  $scope.perro.ultimo = time;
+          
 		  var jsonjugador = JSON.stringify($scope.jugador);
 		  var json = JSON.stringify($scope.perro);
 		localStorage.setItem('jugador',jsonjugador);
